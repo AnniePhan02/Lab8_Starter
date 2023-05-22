f277f0860950d12e12610408255e3924e5e4a717
AnniePhan02 <a5phan@ucsd.edu>
2023-05-21 23:54:36 -0700

    my unit tests included

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ Annie Phan
 
 1. I would fit my automated tests within a Github action that runs whenever code is pushed because it would test and verify my code as I am developing it. Based on the fedback that I get from automated tests, I would then adjust my code accordingly. Fitting automated tests within GitHub actions would help automate the process. If I were to manually run them before pushing code, this is inefficient and GitHub actions could do these tasks for me. I would not run automated tests after all development is completed because I would not be able to catch bugs earlier on and I might have to re-write a significant portion of the development.
 2.  No, I would not use an end to end test too check if a function is returning the correct output because I would use a unit test instead. 
-3.  
+3.  No, I would not use a unit test to test the message feature of a messaging application because I would have to know how the message interacts with other components of my application. While unit testing would allow me to test a specific part of my message, I would not be able to see the rest of it.
+4.  I would use a unit test to test the "max message length" feature of a messaging application because it is a feature of my message and does not impact/interact with other parts of my application. This is a specific use of my message and is isolated enough to test.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,79 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+// Phone number
+test('phone number is correct', () => {
+    expect(functions.isPhoneNumber('123-456-7890')).toBe(true);
+});
+
+test('phone number 2 is correct', () => {
+    expect(functions.isPhoneNumber('098-765-4321')).toBe(true);
+});
+
+test('phone number 3 is incorrect', () => {
+expect(functions.isPhoneNumber('911')).toBe(true);
+});
+
+test('phone number 4 is incorrect', () => {
+expect(functions.isPhoneNumber('1234-213-532')).toBe(true);
+});
+
+// Valid email
+test('email is correct', () => {
+expect(functions.isEmail('a5phan@ucsd.edu')).toBe(true);
+});
+test('email 2 is correct', () => {
+    expect(functions.isEmail('a@p.com')).toBe(true);
+});
+test('email 3 is incorrect', () => {
+    expect(functions.isEmail('a@pcom')).toBe(true);
+});
+test('email 4 is incorrect', () => {
+    expect(functions.isEmail('AnniePhan')).toBe(true);
+});
+
+// Strong Password
+test('strong password is correct', () => {
+expect(functions.isStrongPassword('a12345')).toBe(true);
+});
+test('strong password 2 is correct', () => {
+    expect(functions.isStrongPassword('ucsd123')).toBe(true);
+});
+test('strong password 3 is incorrect', () => {
+    expect(functions.isStrongPassword('1234567890ucsd')).toBe(true);
+});
+test('strong password 4 is incorrect', () => {
+    expect(functions.isStrongPassword('abc')).toBe(true);
+});
+
+// is Date
+test('Date 1 is correct', () => {
+    expect(functions.isDate('12/16/2002')).toBe(true);
+});
+test('Date 2 is correct', () => {
+    expect(functions.isDate('1/1/2000')).toBe(true);
+});
+test('Date 3 is correct', () => {
+    expect(functions.isDate('22/22/22')).toBe(true);
+});
+test('Date 4 is correct', () => {
+    expect(functions.isDate('05/21')).toBe(true);
+});
+
+// is HexColor
+test('HexColor 1 is correct', () => {
+    expect(functions.isHexColor('#fff')).toBe(true);
+});
+test('HexColor 2 is correct', () => {
+    expect(functions.isHexColor('0AC')).toBe(true);
+});
+test('HexColor 1 is correct', () => {
+    expect(functions.isHexColor('zzz')).toBe(true);
+});
+test('HexColor 1 is correct', () => {
+    expect(functions.isHexColor('#ffff')).toBe(true);
+});
+
+
+
+
